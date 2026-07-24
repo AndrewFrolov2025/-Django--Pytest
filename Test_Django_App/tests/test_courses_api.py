8ed5aa0005b31286b48d06bd32b4a2169cfda5a7
AndrewFrolov2025 <amurartstudio@gmail.com>
2026-07-25 00:46:35 +0900

    Update test_courses_api.py

--- Test_Django_App/tests/test_courses_api.py
+++ Test_Django_App/tests/test_courses_api.py
@@ -1,12 +1,20 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
 from rest_framework import status
 from rest_framework.test import APIClient
-from students.models import Course, Student
+from students.models import Course
 
 @pytest.fixture
 def client():
     return APIClient()
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
 
 @pytest.mark.django_db
 def test_get_courses_list(client):
@@ -62,4 +70,25 @@ def test_delete_course(client):
     response = client.delete(url)
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert Course.objects.count() == 0
+
+@pytest.mark.django_db
+def test_get_one_course(client, course_factory):
+    course = course_factory(name="1C - programmer")
+    url = reverse("courses-detail", args=[course.id])
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["id"] == course.id
+    assert response.data["name"] == course.name
+
+@pytest.mark.django_db
+def test_filter_courses_by_id(client, course_factory):
+    course_1 = course_factory(name="Python")
+    course_2 = course_factory(name="Django")
+    url = reverse("courses-list")
+    response = client.get(url, data={"id": course_1.id})
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.data) == 1
+    assert response.data[0]["id"] == course_1.id
+    assert response.data[0]["name"] == course_1.name
+    assert response.data[0]["id"] != course_2.id
 
